f5785633379be44dbb7f914fc9e0b6d22c3e01bb
EnesT51 <99040669@mydavinci.nl>
2021-09-10 14:18:47 +0200

    person

---
+++ person.py
@@ -0,0 +1,19 @@
+naam = input('wat is je naam')
+print(naam)
+
+adres = input('wat is je adres')
+print(adres)
+
+postcode = input ('wat is je postcode')
+print(postcode)
+
+woonplaats = input ('waar kom je vandaan')
+print(woonplaats)
+
+
+print ("----------------------------------------------------")
+print ("|  Naam      : " + naam)
+print ("|  Adres     : " + adres)
+print ("|  Postcode  : " + postcode)
+print ("|  Woonplaats: " + woonplaats)
+print ("----------------------------------------------------")
