9821c6d95191b3b827eb130ea10f5d0c4fb9730c
EnesT51 <99040669@mydavinci.nl>
2021-09-14 14:44:14 +0200

    inuptoutput

--- feestlunch.py
+++ feestlunch.py
@@ -3,7 +3,7 @@ hoeveelcroissantje = int(input("hoeveel croissant koop je?: "))
 hoeveelstokbrood =  int(input("hoeveel stokbrood koop je?: "))
 Prijs = float (2.78)
 kortingbonperstuk = float (0.50)
-hoeveelbonnen = int(input("hoeveel kortingsbonnen gebruik je?:"))
+hoeveelbonnen = int(input("hoeveel kortingsbonnen gebruik je?: "))
  
 print (int(((hoeveelcroissantje * prijs) + (hoeveelstokbrood * Prijs) - (kortingbonperstuk * hoeveelbonnen))*100),"cent") 
 
